32a50f6c59996a91935316750087274b361987ea
adriano netto <adriano.netto2010@gmail.com>
2021-12-09 12:35:58 -0300

    relations

--- app/Models/Plano.php
+++ app/Models/Plano.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Plano extends Model
 {
     use HasFactory;
+
+    public function tipo_pagamentos(): BelongsToMany
+    {
+        return $this->belongsToMany(TipoPagamento::class);
+    }
 }
--- app/Models/TipoPagamento.php
+++ app/Models/TipoPagamento.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class TipoPagamento extends Model
 {
     use HasFactory;
+
+    public function planos(): BelongsToMany
+    {
+        return $this->belongsToMany(Plano::class);
+    }
 }
