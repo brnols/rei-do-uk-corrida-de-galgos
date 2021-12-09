e20226262c3c28828ebaad0cffbc3c6db5623af6
adriano netto <adriano.netto2010@gmail.com>
2021-12-09 13:36:29 -0300

    faturas relations

--- app/Models/Fatura.php
+++ app/Models/Fatura.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Fatura extends Model
 {
     use HasFactory;
+
+    public function contrato(): BelongsTo
+    {
+        return $this->belongsTo(Contrato::class);
+    }
 }
