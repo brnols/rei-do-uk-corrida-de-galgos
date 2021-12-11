5c083a0be24e03f07f2789c0c5abb4a24eff274a
adriano netto <adriano.netto2010@gmail.com>
2021-12-10 21:14:40 -0300

    cadastro

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,16 +1,16 @@
 <template>
     <Head title="Login"/>
 
-    <h1>Login</h1>
+    <h1 class="h3 text-primary text-center">
+        Login
+    </h1>
 
     <p class="text-primary text-center mt-2">
         Seja bem vindo! Acesse agora toda a potência em análise de Galgos UK.
     </p>
 
-    <q-form
-        @submit.prevent="submit"
-        class="flex flex-col space-y-6 q-pb-lg"
-    >
+    <q-form @submit.prevent="submit" class="flex flex-col space-y-6 pb-4">
+
         <q-input
             type="email"
             v-model="form.email"
@@ -24,6 +24,7 @@
             :error="!!errors.email"
             :error-message="errors.email"
         />
+
         <q-input
             type="password"
             v-model="form.password"
@@ -34,12 +35,14 @@
             rounded
             dense
         />
+
         <q-checkbox
             class="ml-4 text-primary"
             v-model="form.remember"
             label="Lembrar"
             dense
         ></q-checkbox>
+
         <q-btn
             type="submit"
             class="mx-auto normal-case w-32"
@@ -49,13 +52,16 @@
             :loading="form.processing"
             :disabled="form.processing"
         ></q-btn>
+
     </q-form>
-    <span>
+
+    <span class="justify-center">
         Ainda não tem cadastro?
         <Link :href="route('register')" class="text-primary">
             Cadastre-se aqui!
         </Link>
     </span>
+
     <div class="mt-3 flex justify-center items-center">
         <img src="/images/icons/telegram.png" alt="telegram" class="mr-2">
         <a href="#" target="_blank" class="text-primary">
@@ -106,18 +112,5 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-    text-align: center;
-    font-weight: bold;
-    font-size: 50px;
-    line-height: 60px;
-    color: #0342a1;
-}
 
-.card {
-    max-width: 420px;
-    border: 0.5px solid #eaebed;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 30px;
-}
 </style>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,59 +1,90 @@
 <template>
-    <Head title="Register" />
+    <Head title="Cadastro" />
 
-    <BreezeValidationErrors class="mb-4" />
+    <h1 class="h3 text-primary text-center">
+        Cadastro
+    </h1>
 
-    <form @submit.prevent="submit">
-        <div>
-            <BreezeLabel for="name" value="Name" />
-            <BreezeInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
-        </div>
+    <q-form @submit.prevent="submit" class="flex flex-col space-y-6 pb-4">
 
-        <div class="mt-4">
-            <BreezeLabel for="email" value="Email" />
-            <BreezeInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autocomplete="username" />
-        </div>
+        <q-input
+            :hide-bottom-space="!errors.name"
+            :error="!!errors.name"
+            :error-message="errors.name"
+            v-model="form.name"
+            placeholder="Nome"
+            dense
+            outlined
+            rounded
+        ></q-input>
 
-        <div class="mt-4">
-            <BreezeLabel for="password" value="Password" />
-            <BreezeInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="new-password" />
-        </div>
+        <q-input
+            type="email"
+            :hide-bottom-space="!errors.email"
+            :error="!!errors.email"
+            :error-message="errors.email"
+            v-model="form.email"
+            placeholder="E-mail"
+            dense
+            outlined
+            rounded
+        ></q-input>
 
-        <div class="mt-4">
-            <BreezeLabel for="password_confirmation" value="Confirm Password" />
-            <BreezeInput id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
-        </div>
+        <q-input
+            type="password"
+            :hide-bottom-space="!errors.password"
+            :error="!!errors.password"
+            :error-message="errors.password"
+            v-model="form.password"
+            placeholder="Senha"
+            dense
+            outlined
+            rounded
+        ></q-input>
 
-        <div class="flex items-center justify-end mt-4">
-            <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                Already registered?
-            </Link>
+        <q-input
+            type="password"
+            :hide-bottom-space="!errors.password_confirmation"
+            :error="!!errors.password_confirmation"
+            :error-message="errors.password_confirmation"
+            v-model="form.password_confirmation"
+            placeholder="Confirme a senha"
+            dense
+            outlined
+            rounded
+        ></q-input>
 
-            <BreezeButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Register
-            </BreezeButton>
-        </div>
-    </form>
+        <q-btn
+            type="submit"
+            class="mx-auto normal-case w-32"
+            color="primary"
+            rounded
+            label="Enviar"
+            :loading="form.processing"
+            :disabled="form.processing"
+        ></q-btn>
+
+        <Link :href="route('login')" class="text-center text-primary">
+            Já sou cadastrado
+        </Link>
+
+    </q-form>
 </template>
 
 <script>
-import BreezeButton from '@/Components/Button.vue'
-import BreezeGuestLayout from '@/Layouts/Auth.vue'
-import BreezeInput from '@/Components/Input.vue'
-import BreezeLabel from '@/Components/Label.vue'
-import BreezeValidationErrors from '@/Components/ValidationErrors.vue'
+import Auth from '@/Layouts/Auth.vue'
 import { Head, Link } from '@inertiajs/inertia-vue3';
 
 export default {
-    layout: BreezeGuestLayout,
+    layout: Auth,
 
     components: {
-        BreezeButton,
-        BreezeInput,
-        BreezeLabel,
-        BreezeValidationErrors,
         Head,
         Link,
+    },
+
+    props: {
+        errors: Object
     },
 
     data() {
